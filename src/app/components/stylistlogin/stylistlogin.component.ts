259592cebf2a3e73565e515f8814708a6cc16bd0
Gayashan <gayashan.k@eyepax.com>
2019-01-21 11:21:41 +0530

    table added to the stylist login

--- src/app/components/stylistlogin/stylistlogin.component.ts
+++ src/app/components/stylistlogin/stylistlogin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../Services/authentication.service';
+import {Sort} from '@angular/material';
 
 @Component({
   selector: 'app-stylistlogin',
@@ -9,6 +10,14 @@ import { AuthenticationService } from '../../Services/authentication.service';
 export class StylistloginComponent implements OnInit {
   details;
   checked = false;
+
+   bookings = [
+    {salon: 'Frozen yogurt', date: '159', city: '6', street: '24', telephone: '4'},
+    {salon: 'Ice cream sandwich',  date: '237', city: '9', street: '37', telephone: '4'},
+    {salon: 'Eclair',  date: '262', city: '16', street: '24', telephone: '6'},
+    //{name: 'Cupcake', calories: '305', fat: '4', carbs: '67', protein: '4'},
+    //{name: 'Gingerbread', calories: '356', fat: '16', carbs: '49', protein: '4'},
+  ];
 
   constructor( private auth: AuthenticationService ) { }
 
