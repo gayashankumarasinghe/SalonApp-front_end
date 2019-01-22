36df1c37eac952b308ef336735e37643ff4a950b
Gayashan <gayashan.k@eyepax.com>
2019-01-22 12:35:17 +0530

    Things wanted to edit stylist form added

--- src/app/Services/salon.service.ts
+++ src/app/Services/salon.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { map, catchError, tap } from 'rxjs/operators';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+
 
 @Injectable()
 export class SalonService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  public getAllSalons() {
+    //return this.http.get<User[]>(this.userUrl);
+    return this.http.get('/all');
+  }
+
+   public updateSalon(salonId) {
+    //return this.http.get<User[]>(this.userUrl);
+    return this.http.get('/all');
+  }
+
+
+
+
 
 }
