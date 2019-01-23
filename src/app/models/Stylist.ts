dfe7fe62a0dcf57be48dee4a971818e8b2cd4ede
Gayashan <gayashan.k@eyepax.com>
2019-01-23 09:21:31 +0530

    models added stylist and salon

---
+++ src/app/models/Stylist.ts
@@ -0,0 +1,13 @@
+export interface Stylist{
+    id?:string;
+    name?:string;
+    country?:string;
+    //city_id?:String; //integer
+    description?:string;
+    street_address?:string;
+    website?:string;
+    facebook?:string;
+    postal_code?:string; //integer
+    email?:string;
+  
+}
