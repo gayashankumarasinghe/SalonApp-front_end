55f10d79ef5f3c16ad114bbc8933ad2c44ac4fef
Gayashan <gayashankaushallya@gmail.com>
2019-01-27 09:41:41 +0530

    add the profile pic and update the check availability pop up

--- src/app/Services/authentication.service.ts
+++ src/app/Services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 import { map, catchError, tap } from 'rxjs/operators';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
@@ -8,7 +9,9 @@ import 'rxjs/add/observable/throw';
 @Injectable()
 export class AuthenticationService {
 
-  constructor( private http: HttpClient ) { }
+  authenticated = false;
+
+  constructor( private http: HttpClient, private router: Router ) { }
 
   registerStylist(token:any): Observable<any>{
     return this.http.post('/salon/add',token);
@@ -29,5 +32,28 @@ export class AuthenticationService {
   searchFreeDate(token:any): Observable<any>{
     return this.http.post('/api/booking/v1/check-booking',token)
   }
+
+  authentication(credentials, callback) {
+
+    const headers = new HttpHeaders(credentials ? {
+        authorization : 'Basic ' + btoa(credentials.username + ':' + credentials.password)
+    } : {});
+
+    this.http.get('/api/login/user', {headers: headers}).subscribe(response => {
+        if (response['name']) {
+            this.authenticated = true;
+        } else {
+            this.authenticated = false;
+        }
+        return callback && callback();
+    });
+  }
+
+  // logout() {
+  //   this.http.post('logout', {}).finally(() => {
+  //       this.authenticated = false;
+  //       this.router.navigateByUrl('/');
+  //   }).subscribe();
+  // }
 
 }
